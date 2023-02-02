e9103fddaf64d3dee19538e8f57bff6b9bf2f425
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-01 16:33:04 -0800

    rename puppy photo to match html

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new ESLintPlugin(),
@@ -30,7 +30,44 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(gif|png|avid|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
       }
     ]
   }
 };
+
+
+
+
+
+
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ]
+//       }
+//     ]
+//   }
+// };
