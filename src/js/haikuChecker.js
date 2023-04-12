52821ca09b0844cd319932dd304bc27e3ed9e162
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-04-12 15:28:52 -0700

    add edge case functions
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>
    Co-authored-by: David Jandron <jandron1211@gmail.com>
    Co-authored-by: John <john.lenz27@gmail.com>
    Co-authored-by: E Luckie <eluckie.d@gmail.com>
    Co-authored-by: Mesha Devan <madsketchz@gmail.com>

--- src/js/haikuChecker.js
+++ src/js/haikuChecker.js
@@ -2,43 +2,75 @@
 export const countVowels = (string) => {
   const vowels = ["a", "e", "i", "o", "u", "y"];
   let counter = 0;
-  for (let i = 0; i < string.length; i++){
-    if(vowels.indexOf(string[i].toLowerCase()) !== -1){
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.indexOf(string[i].toLowerCase()) !== -1) {
       counter++;
     }
   }
   return counter;
 };
-
-// export const countDif = (string) => {
-//   const difVowels = ["oo", "ui", "ea", "ay", "ae", "oi", "ou", "oa", "ee", "ai", "oy", "ey", "ye", "ei", "you" ,"ya"];
-//   let counter = 0;
-//   for (let i = 0; i < string.length; i++){
-//     if(difVowels.indexOf(string[i].toLowerCase()) !== -1){
-//       counter++;
-//     }
-//   }
-//   return counter;
-// };
 
 export const countDif = (string) => {
   const vowels = ["a", "e", "i", "o", "u", "y"];
   let counter = 0;
-  for (let i = 0; i < string.length; i++){
+  for (let i = 0; i < string.length; i++) {
     const currentChar = string[i];
     const nextChar = string[i + 1];
-    if(vowels.includes(currentChar) && vowels.includes(nextChar)){
+    if (vowels.includes(currentChar) && vowels.includes(nextChar)) {
       counter++;
     }
   }
   return counter;
 };
 
-// export const countDif = (string) => {
-//   const result = (/[aeiouy][aeiouy]/gi)
-// }
+export const hiddenVowel = (string) => {
+  const vowels = ["e"];
+  let counter = 0;
+  for (let i = 0; i < string.length; i++) {
+    const currentChar = string[i];
+    const nextChar = string[i + 1];
+    const lastChar = string.charAt(string.length - 1);
+    if (vowels.includes(currentChar) && (nextChar === " ")) {
+      counter++;
+    }
+    if (lastChar.includes(vowels) && !nextChar) {
+      counter++;
+    }
+  }
+  return counter;
+};
 
+export const silentCandle = (string) => {
+  let counter = 0;
+  const splitString = string.split(" ");
+  splitString.forEach(word => {
+    if (word.endsWith('le')) {
+      counter++;
+    }
+  });
+  return counter;
+};
+
+export const silentEs = (string) => {
+  let counter = 0;
+  const splitString = string.split(" ");
+  splitString.forEach(word => {
+    if (word.endsWith('hes')) {
+      counter--;
+    }
+    if (word.endsWith('xes')){
+      counter--;
+    }
+    if (word.endsWith('ses')){
+      counter--;
+    }
+    if (word.endsWith('es')) {
+      counter++;
+    }
+  }); 
+  return counter;
+};
 
 export const countSyllables = (string) => {
-  return countVowels(string) - countDif(string);
+  return countVowels(string) - countDif(string) - hiddenVowel(string) + silentCandle(string) - silentEs(string);
 };
